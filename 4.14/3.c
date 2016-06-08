686f6b2520a2641232d0ee3910ed2a2bff4cb439
weijunzhou <20044439@163.com>
2016-06-08 08:34:34 +0800

    求素数方法
    Signed-off-by: weijunzhou <20044439@163.com>

---
+++ 4.14/3.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 3.c
+	> Author: weijun
+	> Mail: 20044439@163.com 
+	> Created Time: 2016年06月 7日 10:05:58
+ ************************************************************************/
+
+#include<stdio.h>
+int main(void)
+{
+	int x, y, z;
+
+	printf("please input three numbers as side of triangle:\n");
+	scanf("%d %d %d", &x,&y,&z);
+	
